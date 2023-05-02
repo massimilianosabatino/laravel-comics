bf82073b9610a21a176ef4c017bcecbd3e465bfc
massimilianosabatino <massimiliano.sabatino@gmail.com>
2023-05-03 00:23:22 +0200

    add: main element

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,4 +1,19 @@
 @extends('layout.app')
+
 @section('page.main')
-    <p>test</p>
+<section id="jumbo"></section>
+<section id="content">
+    <div class="container-home">
+        <div class="tag">CURRENT SERIES</div>
+        <section class="current">
+            @foreach ($comics as $comic)
+                <div class="single">
+                    <img :src="{{ $comic['thumb'] }}" :alt="{{ $comic['title']}}">
+                    <h2>{{ $comic['title'] }}</h2>
+                </div>
+            @endforeach
+        </section>
+        <button type="button" class="load-more">LOAD MORE</button>
+    </div>
+</section>
 @endsection
