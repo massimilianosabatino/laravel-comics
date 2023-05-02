40a7be491f83c3b033dfea7670efb3509af68a32
massimilianosabatino <massimiliano.sabatino@gmail.com>
2023-05-03 01:15:45 +0200

    add: footer menu

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,3 +1,32 @@
 <footer>
-
+    <div class="bg">
+        <div class="secondary-menu">
+            <div class="menu">
+                @foreach ($menu as $item)    
+                <ul v-for="section in menu" class="category">
+                    <li>
+                        <a href="{{$item['link']}}" target="{{$item['target']}}">
+                            {{ $item['section'] }}
+                        </a>
+                        <ul class="link">
+                            
+                            @foreach ($item['linkList'] as $link)    
+                            <li>
+                                <a href="{{$link['link']}}" target="{{$link['target']}}">
+                                    {{$link['name']}}
+                                </a>
+                            </li>
+                            @endforeach
+                        </ul>
+                    </li>
+                </ul>
+                @endforeach
+            </div>
+            <div class="logo-img">
+                <img src="{{ Vite::asset('resources/img/dc-logo-bg.png') }}" alt="Logo DC">
+                
+            </div>
+        </div>
+    </div>
+    <FooterActionsComponents />
 </footer>
--- routes/web.php
+++ routes/web.php
@@ -43,7 +43,162 @@ Route::get('/', function () {
                 'url' => asset('resources/img/buy-dc-power-visa.svg'),
                 'target' => '#'
             ],
-        ]
+        ],
+        'menu' => [
+                [
+                    'section'=> 'DC COMICS',
+                    'link'=> '#',
+                    'target'=> '_self',
+                    'linkList'=> [
+                        [
+                            'name'=> 'Characters',
+                            'link'=> '#',
+                            'target'=> '_self'
+                        ],
+                        [
+                            'name'=> 'Comics',
+                            'link'=> '#',
+                            'target'=> '_self'
+                        ],
+                        [
+                            'name'=> 'Movies',
+                            'link'=> '#',
+                            'target'=> '_self'
+                        ],
+                        [
+                            'name'=> 'TV',
+                            'link'=> '#',
+                            'target'=> '_self'
+                        ],
+                        [
+                            'name'=> 'Games',
+                            'link'=> '#',
+                            'target'=> '_self'
+                        ],
+                        [
+                            'name'=> 'Videos',
+                            'link'=> '#',
+                            'target'=> '_self'
+                        ],
+                        [
+                            'name'=> 'News',
+                            'link'=> '#',
+                            'target'=> '_self'
+                        ],
+                    ]
+                ],
+                [
+                    'section'=> 'SHOP',
+                    'link'=> '#',
+                    'target'=> '_self',
+                    'linkList'=> [
+                        [
+                            'name'=> 'Shop DC',
+                            'link'=> '#',
+                            'target'=> '_self'
+                        ],
+                        [
+                            'name'=> 'Shop DC Collectibles',
+                            'link'=> '#',
+                            'target'=> '_self'
+                        ],
+                    ]
+                ],
+                [
+                    'section'=> 'DC',
+                    'link'=> '#',
+                    'target'=> '_self',
+                    'linkList'=> [
+                        [
+                            'name'=> 'Terms Of Use',
+                            'link'=> '#',
+                            'target'=> '_self'
+                        ],
+                        [
+                            'name'=> 'Privacy policy (New)',
+                            'link'=> '#',
+                            'target'=> '_self'
+                        ],
+                        [
+                            'name'=> 'Ad Choices',
+                            'link'=> '#',
+                            'target'=> '_self'
+                        ],
+                        [
+                            'name'=> 'Advertising',
+                            'link'=> '#',
+                            'target'=> '_self'
+                        ],
+                        [
+                            'name'=> 'Jobs',
+                            'link'=> '#',
+                            'target'=> '_self'
+                        ],
+                        [
+                            'name'=> 'Subscriptions',
+                            'link'=> '#',
+                            'target'=> '_self'
+                        ],
+                        [
+                            'name'=> 'Talent Workshops',
+                            'link'=> '#',
+                            'target'=> '_self'
+                        ],
+                        [
+                            'name'=> 'CPSC Certificates',
+                            'link'=> '#',
+                            'target'=> '_self'
+                        ],
+                        [
+                            'name'=> 'Ratings',
+                            'link'=> '#',
+                            'target'=> '_self'
+                        ],
+                        [
+                            'name'=> 'Shop Help',
+                            'link'=> '#',
+                            'target'=> '_self'
+                        ],
+                        [
+                            'name'=> 'Contact Us',
+                            'link'=> '#',
+                            'target'=> '_self'
+                        ],
+                    ]
+                ],
+                [
+                    'section'=> 'SITES',
+                    'link'=> '#',
+                    'target'=> '_self',
+                    'linkList'=> [
+                        [
+                            'name'=> 'DC',
+                            'link'=> '#',
+                            'target'=> '_self'
+                        ],
+                        [
+                            'name'=> 'MAD Magazine',
+                            'link'=> '#',
+                            'target'=> '_self'
+                        ],
+                        [
+                            'name'=> 'DC Kids',
+                            'link'=> '#',
+                            'target'=> '_self'
+                        ],
+                        [
+                            'name'=> 'DC Universe',
+                            'link'=> '#',
+                            'target'=> '_self'
+                        ],
+                        [
+                            'name'=> 'DC Power Visa',
+                            'link'=> '#',
+                            'target'=> '_self'
+                        ],
+                    ]
+                ],
+            ]
     ];
     
     return view('home', $data);
