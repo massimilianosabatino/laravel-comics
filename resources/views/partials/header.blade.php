80a7ae284a396a5e12ebb1b1077dd8de5cfc59c3
massimilianosabatino <massimiliano.sabatino@gmail.com>
2023-05-02 23:54:56 +0200

    fix: menu list item

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -6,15 +6,34 @@
         <ul class="menu">
             <li>
                 <a href="{{ route('home') }}">characters</a>
+            </li>
+            <li>
                 <a href="{{ route('home') }}">comics</a>
+            </li>
+            <li>
                 <a href="{{ route('home') }}">movies</a>
+            </li>
+            <li>
                 <a href="{{ route('home') }}">tv</a>
+            </li>
+            <li>
                 <a href="{{ route('home') }}">games</a>
+            </li>
+            <li>
                 <a href="{{ route('home') }}">collectibles</a>
+            </li>
+            <li>
                 <a href="{{ route('home') }}">videos</a>
+            </li>
+            <li>
                 <a href="{{ route('home') }}">fans</a>
+            </li>
+            <li>
                 <a href="{{ route('home') }}">news</a>
+            </li>
+            <li>
                 <a href="{{ route('home') }}">shop</a>
+            </li>
             </li>
         </ul>
     </div>
