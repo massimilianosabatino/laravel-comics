e80774cd9e6c35ea5b5d17c668a0482ccec6a90d
massimilianosabatino <massimiliano.sabatino@gmail.com>
2023-05-02 23:02:27 +0200

    add: folder structure

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -4,10 +4,21 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Template Blade and Vite</title>
+    <title>@yeld('page.title')</title>
     @vite('resources/js/app.js')
 </head>
 <body>
+    @include('partials.header')
+    <header>
+
+    </header>
+    <main>
+        @yeld('page.main')
+    </main>
     <h1>Template Blade and Vite</h1>
+    @include('partials.footer')
+    <footer>
+
+    </footer>
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+
+    $data = [
+        'comics' => config('comics'),
+    ];
+    
+    return view('home', $data);
 });
