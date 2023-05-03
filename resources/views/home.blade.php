2e04fc12676c30b17512a7c3643692a6ec96cbcd
massimilianosabatino <massimiliano.sabatino@gmail.com>
2023-05-03 17:15:26 +0200

    add: bootstrap to comic in home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,34 +1,55 @@
 @extends('layout.app')
 
 @section('page.main')
+{{-- Jumbo --}}
 <section id="jumbo"></section>
+{{-- /Jumbo --}}
+{{-- Main content --}}
 <section id="content">
-    <div class="container-home">
+    <div class="container-card container">
+        {{-- Tag serie name --}}
         <div class="tag">CURRENT SERIES</div>
-        <section class="current">
+        {{-- /Tag serie name --}}
+        {{-- Comics card --}}
+        <div class="row row-cols-1 row-cols-md-6 g-4 current">
             @foreach ($comics as $comic)
-                <div class="single">
-                    <img src="{{ $comic['thumb'] }}" alt="{{ $comic['title']}}">
-                    <h2>{{ $comic['title'] }}</h2>
+            <div class="col">
+                {{-- Single comic --}}
+                <div class="card bg-transparent border-0 h-100">
+                    {{-- Comic image --}}
+                    <img src="{{ $comic['thumb'] }}" class="card-img-top img-fluid" alt="{{ $comic['title']}}">
+                    {{-- /Comic image --}}
+                    <div class="card-body">
+                        {{-- Comic title --}}
+                        <h5 class="card-title">{{ $comic['title'] }}</h5>
+                        {{-- /Comic title --}}
+                    </div>
                 </div>
+                {{-- /Single comic --}}
+            </div>
             @endforeach
-        </section>        
+        </div>
+        {{-- /Comics card --}}
+        {{-- Pagination --}}
         <button type="button" class="load-more">LOAD MORE</button>
-    </div>
+        {{-- /Pagination --}}
+    </section>
 </section>
+{{-- /Main content --}}
+{{-- Secondary menu --}}
 <section>
     <div class="sub-menu">
         <div class="wrapper">
-                @foreach ($cards as $item)    
-                <a href="{{ $item['target'] }}">
-                    <div>
-                        <img src="{{ $item['url'] }}" alt="Icon {{ $item['name'] }}">
-                        <div class="text">{{ $item['name'] }}</div>
-                    </div>
-                </a>
-                @endforeach
+            @foreach ($cards as $item)    
+            <a href="{{ $item['target'] }}">
+                <div>
+                    <img src="{{ $item['url'] }}" alt="Icon {{ $item['name'] }}">
+                    <div class="text">{{ $item['name'] }}</div>
+                </div>
+            </a>
+            @endforeach
         </div>
-        <!-- </div> -->
     </div>
 </section>
+{{-- /Secondary menu --}}
 @endsection
