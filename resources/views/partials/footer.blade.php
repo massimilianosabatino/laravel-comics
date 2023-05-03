d43a119dcca84e5f1ea714175b71e4cbafac9c26
massimilianosabatino <massimiliano.sabatino@gmail.com>
2023-05-03 20:16:33 +0200

    add: bootstrap on secondary menu

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,7 +1,7 @@
 <footer>
-    <div class="bg">
+    {{-- <section class="bg">
         <div class="secondary-menu">
-            <div class="menu">
+            <div class="menu container">
                 @foreach ($menu as $item)    
                 <ul class="category">
                     <li>
@@ -27,7 +27,42 @@
                 
             </div>
         </div>
-    </div>
+    </section> --}}
+    <section class="bg">
+        <div class="secondary-menu container">
+            <div class="menu h-100">
+                <div class="category  h-100">
+                    <div class="row flex-column h-100">
+                        @foreach ($menu as $item)    
+                        <div class="col-4">
+                            <a href="{{$item['link']}}" target="{{$item['target']}}">
+                                {{ $item['section'] }}
+                            </a>
+                            <div class="link">
+                                
+                                @foreach ($item['linkList'] as $link)    
+                                <div>
+                                    <a href="{{$link['link']}}" target="{{$link['target']}}">
+                                        {{$link['name']}}
+                                    </a>
+                                </div>
+                                @endforeach
+                            </div>
+                        </div>
+                        @endforeach
+                    </div>
+                    <div class="disclaimer">
+                        <p>
+                            All Site Content TM and &copy 2020 DC Entertainment, unless otherwise <a href="#">noted here</a>. All right reserved. <a href="#">Cookies Settings</a>
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="logo-img">
+                <img src="{{ Vite::asset('resources/img/dc-logo-bg.png') }}" alt="Logo DC">
+            </div>
+        </div>
+    </section>
     <div class="bg-actions">
         <div class="ms-container">
             <button type="button">SIGN-UP NOW!</button>
