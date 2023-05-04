103cf4567fe45aa074529f421bd3b8caca534f62
massimilianosabatino <massimiliano.sabatino@gmail.com>
2023-05-04 02:16:14 +0200

    add: favicon and comment

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -5,6 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('page.title')</title>
+    <link rel="icon" type="image/x-icon" href="{{ Vite::asset('resources/img/favicon.ico')}}">
     @vite('resources/js/app.js')
 </head>
 <body>
--- resources/views/title.blade.php
+++ resources/views/title.blade.php
@@ -18,32 +18,41 @@
     <div class="container">
         <div class="row">
             <div class="col-8">
+                {{-- Comic title --}}
                 <h1>{{ $comics['title'] }}</h1>
+                {{-- /Comic title --}}
+                {{-- Price bar --}}
                 <div class="d-flex align-items-center border-bottom stock">
-                        <div class="left-container d-flex justify-content-between flex-fill">
-                            <div class="price px-4">
-                                <span>U.S. Price:</span> {{ $comics['price'] }}
-                            </div>
-                            <div class="available px-4">
-                                <span>AVAILABLE</span>
-                            </div>
+                    {{-- Left container --}}
+                    <div class="left-container d-flex justify-content-between flex-fill">
+                        <div class="price px-4">
+                            <span>U.S. Price:</span> {{ $comics['price'] }}
                         </div>
-                        <div class="dropdown border-start ms-auto">
-                            <button class="btn rounded-0 dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
-                                Chech Availablity
-                            </button>
-                            <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
-                                <li><a class="dropdown-item" href="#">Action</a></li>
-                                <li><a class="dropdown-item" href="#">Another action</a></li>
-                                <li><a class="dropdown-item" href="#">Something else here</a></li>
-                            </ul>
+                        <div class="available px-4">
+                            <span>AVAILABLE</span>
                         </div>
+                    </div>
+                    {{-- /Left container --}}
+                    {{-- Right container - dropdown --}}
+                    <div class="dropdown border-start ms-auto">
+                        <button class="btn rounded-0 dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+                            Chech Availablity
+                        </button>
+                        <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+                            <li><a class="dropdown-item" href="#">Action</a></li>
+                            <li><a class="dropdown-item" href="#">Another action</a></li>
+                            <li><a class="dropdown-item" href="#">Something else here</a></li>
+                        </ul>
+                    </div>
+                    {{-- /Right container - dropdown --}}
                 </div>
+                {{-- /Price bar --}}
                 <div class="description">
                     <p>{{ $comics['description'] }}</p>
                 </div>
                 
             </div>
+            {{-- Sidebar --}}
             <div class="col-4 adv">
                 <h2 class="text-end">ADVERTISEMENT</h3>
                 <div class="text-end"><img src="{{ Vite::asset('resources/img/adv.jpg') }}" alt=""></div>
